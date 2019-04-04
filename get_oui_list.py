bac66b9ca43cfae87e5aad6cdee5a2c21bf0fc38
dooleys23 <dooleys23@gmail.com>
2019-04-04 09:39:24 -0400

    Search for Vendor, write file function.

--- get_oui_list.py
+++ get_oui_list.py
@@ -14,3 +14,27 @@ for row in oui_html_list:
     # EOL hit, expected behavior
     except IndexError as e:
       continue
+
+with open('./path_to_file.txt', 'w+') as cisco_oui_file:
+  for row in oui_master_dic:
+    if 'cisco' in  oui_master_dic[row].lower():
+      cisco_oui_file.write('{0},{1}'.format(row, oui_master_dic[row]))
+      
+'''
+EX: Looking for Cisco MAC's
+>>> for row in oui_master_dic:
+...   if 'cisco' in  oui_master_dic[row].lower():
+...     print row, oui_master_dic[row]
+
+
+EX Output:
+00-0A-41 Cisco Systems, Inc
+00-0A-42 Cisco Systems, Inc
+00-B0-C2 Cisco Systems, Inc
+00-50-3E Cisco Systems, Inc
+00-13-80 Cisco Systems, Inc
+2C-01-B5 Cisco Systems, Inc
+F8-7B-20 Cisco Systems, Inc
+08-96-AD Cisco Systems, Inc
+...
+'''
